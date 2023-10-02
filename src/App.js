5b041c0ffb8d1b73bad81e907df31f1eaac789aa
suraj sachan <100021123+Surajsachan29889@users.noreply.github.com>
2023-10-02 19:43:18 +0530

    logo commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     
 // ********************************
   return (
-<<<<<<< HEAD
+
     <div className='app'>
       {
         
@@ -38,32 +38,10 @@ function App() {
         <Route path='/certification' element={<Certificate/>}/>
       </Routes>
     </Router>
-    }
-    </div>
+  }
+  </div>
       
-=======
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2x9"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
->>>>>>> 48d5e5a3c4ecec754fdc409257892e6c6523c580
+   
   );
 }
 
